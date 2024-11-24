fefe674b2b6190b2db97aa24a8ef013fa42c2a37
Sebasm140111 <sebasm1401@hotmail.com>
2024-11-24 11:20:00 -0500

    Add files via upload

---
+++ css/index.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelector('.slides');
+    const prev = document.querySelector('.prev');
+    const next = document.querySelector('.next');
+    let slideIndex = 0;
+    const totalSlides = document.querySelectorAll('.slides li').length;
+    
+    function showNextSlide() {
+        if (slideIndex < totalSlides - 1) {
+            slideIndex++;
+        } else {
+            slideIndex = 0;
+        }
+        updateSlidePosition();
+    }
+
+    function showPrevSlide() {
+        if (slideIndex > 0) {
+            slideIndex--;
+        } else {
+            slideIndex = totalSlides - 1;
+        }
+        updateSlidePosition();
+    }
+
+    function updateSlidePosition() {
+        slides.style.marginLeft = `-${slideIndex * 100}%`;
+    }
+    next.addEventListener('click', showNextSlide);
+    prev.addEventListener('click', showPrevSlide);
+});
